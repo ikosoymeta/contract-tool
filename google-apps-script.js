1d4f96275fdabdf475a53e7031a8d354ee6e1561
Igor Kosoy <ikosoy@meta.com>
2026-02-25 19:05:05 -0800

    Fix Drive API v3 syntax and add favicon
    - Changed Drive.Files.insert (v2) to Drive.Files.create (v3)
    - Changed title to name, parents format for v3 API
    - Set mimeType to Google Docs for automatic conversion
    - Added inline SVG favicon (blue document icon)

--- google-apps-script.js
+++ google-apps-script.js
@@ -1,37 +1,24 @@
 var FOLDER_ID = '1dcmur1QVcbaPAR0CneVo6DlaA6bq_M6Y';
 
 function doGet(e) {
-  // If no parameters, show status page
   if (!e.parameter.url) {
     return HtmlService.createHtmlOutput('<p>Contract Drafter connector is active.</p>');
   }
-
   try {
     var fileUrl = e.parameter.url;
     var filename = e.parameter.filename || 'document.docx';
     var docTitle = filename.replace(/\.docx$/i, '');
-
-    // Fetch the .docx file from the Cloudflare Worker
     var response = UrlFetchApp.fetch(fileUrl);
     var blob = response.getBlob().setName(filename);
-
-    // Upload to Drive and convert to Google Docs format
     var resource = {
-      title: docTitle,
-      parents: [{ id: FOLDER_ID }]
+      name: docTitle,
+      parents: [FOLDER_ID],
+      mimeType: 'application/vnd.google-apps.document'
     };
-    var file = Drive.Files.insert(resource, blob, { convert: true });
-
+    var file = Drive.Files.create(resource, blob);
     var docUrl = 'https://docs.google.com/document/d/' + file.id + '/edit';
-
-    return HtmlService.createHtmlOutput(
-      '<html><head><meta http-equiv="refresh" content="0;url=' + docUrl + '"></head>'
-      + '<body><p>Google Doc created! Redirecting... <a href="' + docUrl + '">Click here</a> if not redirected.</p></body></html>'
-    );
+    return HtmlService.createHtmlOutput('<html><head><meta http-equiv="refresh" content="0;url=' + docUrl + '"></head><body><p>Google Doc created! <a href="' + docUrl + '">Click here</a> if not redirected.</p></body></html>');
   } catch (err) {
-    return HtmlService.createHtmlOutput(
-      '<html><body><h3>Error creating document</h3><p>' + err.message + '</p>'
-      + '<p><a href="https://drive.google.com/drive/folders/' + FOLDER_ID + '">Open Drive folder</a></p></body></html>'
-    );
+    return HtmlService.createHtmlOutput('<html><body><h3>Error</h3><p>' + err.message + '</p></body></html>');
   }
 }
